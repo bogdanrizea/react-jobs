6100aa8f7082f0e5550d757854949cc0a0863b67
Bogdan Rizea <bogdan.bogdan@sita.aero>
2024-11-11 04:19:33 +0200

    -fixed links -changed data loading from json to json server to mock api -added loading spinner -added 404 page -aded api proxy

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
