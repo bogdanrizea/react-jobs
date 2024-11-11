6a6852689a62d6e34e457200db3d9ef8ac4fc3bb
Bogdan Rizea <bogdan.bogdan@sita.aero>
2024-11-11 05:57:51 +0200

    -delete job and delete page

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -2,11 +2,24 @@ import { useParams, useLoaderData } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm("Are you sure  you want to delte?");
 
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    toast.success("Job delteted!");
+
+    navigate("/jobs");
+  };
   return (
     <>
       <section>
@@ -82,7 +95,10 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
